da775e88d21412398e08e1558eebaa8ebf3d607e
Hanskaspar Frei <hkfrei@karten-werk.ch>
2021-05-05 13:15:40 +0200

    fix: remove build directory, because it is not relevant to repo.

--- build/snowpack.config.js
+++
@@ -1,9 +0,0 @@
-// Using Snowpack's built-in bundling support
-module.exports = {
-  optimize: {
-    bundle: true,
-    minify: true,
-    target: "es2018",
-  },
-  polyfillNode: true, // needed for line-circle-collision package
-};
